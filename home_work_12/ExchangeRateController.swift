db2bddeb68dbfe5d93e60913a9aaf8aea5d6452a
Pavel Bondar <ivizey@gmail.com>
2020-01-18 19:49:31 +0200

    add view cell

---
+++ home_work_12/ExchangeRateController.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRateController.swift
+//  home_work_12
+//
+//  Created by Pavel Bondar on 18.01.2020.
+//  Copyright © 2020 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeRateController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
